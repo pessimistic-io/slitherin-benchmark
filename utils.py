42157a51ee971c135c6e34c66c893d379983cabe
nikolay19 <asterisk89@bk.ru>
2023-12-29 16:47:50 +0100

    get detectors list from slitherin

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import re
 import ast
 import os
 import json
+import slitherin
 
 from collections import namedtuple
 
@@ -9,42 +10,8 @@ from storage import Storage
 
 SOLC_DIR = "solc-bin"
 
-class AbstractDetector:
-    pass  # Your AbstractDetector class definition here
-
-def extract_detectors(folder_path):
-    argument_values = set()
-    for root, _, files in os.walk(folder_path):
-        for file_name in files:
-            if root.endswith("__") or file_name.startswith("__"):
-                continue
-            
-            if file_name.endswith(".py"):
-                file_path = os.path.join(root, file_name)
-                module_path = os.path.splitext(os.path.relpath(file_path, folder_path))[0].replace(os.path.sep, ".")
-
-                with open(file_path, "r") as file:
-                    try:
-                        tree = ast.parse(file.read(), filename=file_path)
-                        for node in ast.walk(tree):
-                            #print(node)
-                            if isinstance(node, ast.ClassDef) and issubclass(AbstractDetector, globals().get(node.name, object)):
-                                for class_node in node.body:
-                                    #if (isinstance(class_node, ast.Assign)):
-                                    #    print("CLASSNAME", class_node.targets[0])
-                                    #print(class_node.name)
-                                    if (
-                                        isinstance(class_node, ast.Assign)
-                                        and len(class_node.targets) == 1
-                                        and isinstance(class_node.targets[0], ast.Name)
-                                        and class_node.targets[0].id == "ARGUMENT"
-                                        and isinstance(class_node.value, ast.Str)
-                                    ):
-                                        argument_values.add((module_path, class_node.value.s))
-                    except SyntaxError as e:
-                        print(f"Error parsing file {file_path}: {e}")
-
-    return list(argument_values)
+def get_slitherin_detectors() -> list:
+    return [detector.ARGUMENT for detector in slitherin.plugin_detectors]
 
 def get_solc_path(comp_ver):
     if comp_ver == None:
@@ -67,7 +34,7 @@ def get_address(filename):
         return "0x"+m.group(1)
 
 Contract = namedtuple('Contract', 'address,chain_id,filename,compiler,detectors')
-DETECTORS = [d[1] for d in extract_detectors(os.path.join("..", "slitherin", "detectors"))]
+DETECTORS = get_slitherin_detectors()
 
 def get_contracts(dir_name, detectors, new_contracts = False, new_detectors = False, limit = None):
     #connect db if need
