6f58f3edb599ae11ce5f77f38759cc18874deb64
nikolay19 <asterisk89@bk.ru>
2024-01-26 21:00:36 +0100

    skip similar & skip libs flags in bench

--- utils.py
+++ utils.py
@@ -45,31 +45,42 @@ def get_solc_dir():
     else:
         raise Exception(f"No solc directory in {SOLC_DIR} for platform {p}")
 
-def get_contracts(dir_name, detectors, new_contracts = False, new_detectors = False, limit = None):
+def get_contracts(dir_name, detectors, new_contracts = False, new_detectors = False, skip_duplicates = False, skip_libs = False, limit = None):
     #connect db if need
     storage = Storage()
     i = 0
-    with open(os.path.join(dir_name, "contracts.json")) as f:
-        for line in f:
-            contract_info = json.loads(line)
-            if new_contracts or new_detectors:
-                detectors_checked = storage.get_contract_detectors(contract_info["address"], contract_info["chain_id"])
-            if new_contracts and len(detectors_checked) > 0:
-                continue #skip contracts checked by any detector
-            if new_detectors:
-                detectors_to_check = [d for d in detectors if d not in detectors_checked]
-            else:
-                detectors_to_check = detectors
-            if len(detectors_to_check) == 0:
-                continue
-            i += 1
-            if limit is not None and i > limit:
-                return
-            yield Contract(
-                contract_info["address"], contract_info["chain_id"],
-                os.path.join(dir_name, contract_info["address"][2:4], contract_info["address"][2:]), 
-                os.path.join(get_solc_dir(), contract_info["compiler"]),
-                detectors_to_check)
+    contracts_fname = os.path.join(dir_name, "contracts.json")
+    if os.path.isfile(contracts_fname):
+        with open(contracts_fname) as f:
+            for line in f:
+                contract_info = json.loads(line)
+                if skip_duplicates and "similar" in contract_info:
+                    continue #skip contracts that have similar contractsv
+                if skip_libs and "lib" in contract_info:
+                    continue #skip contracts that contain only lib files
+                if new_contracts or new_detectors:
+                    detectors_checked = storage.get_contract_detectors(contract_info["address"], contract_info["chain_id"])
+                if new_contracts and len(detectors_checked) > 0:
+                    continue #skip contracts checked by any detector
+                if new_detectors:
+                    detectors_to_check = [d for d in detectors if d not in detectors_checked]
+                else:
+                    detectors_to_check = detectors
+                if len(detectors_to_check) == 0:
+                    continue
+                i += 1
+                if limit is not None and i > limit:
+                    return
+                yield Contract(
+                    contract_info["address"], contract_info["chain_id"],
+                    os.path.join(dir_name, contract_info["address"][2:4], contract_info["address"][2:]), 
+                    os.path.join(get_solc_dir(), contract_info["compiler"]),
+                    detectors_to_check)
+    elif os.path.isdir(dir_name):
+        for project_name in os.listdir(dir_name):
+            project_path = os.path.join(dir_name, project_name)
+            if os.path.isdir(project_path) or project_path.endswith('.sol'):
+                yield Contract("", "", project_path, os.path.join(get_solc_dir(), "v0.8.20+commit.a1b79de6"), detectors)
 
 if __name__ == "__main__":
     folder_path = os.path.join("..", "detectors")
